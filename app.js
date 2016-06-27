547102f48b51c37fcba53da146bd62bbc5c475ea
Elizabeth Legris <elizabeth.legris@gmail.com>
2016-06-27 19:41:02 -0400

    https changes

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ var userLocation
 
 // Detect user location
 
-$.get("http://ipinfo.io", function(response) {
+$.get("https://ipinfo.io", function(response) {
     console.log(response.country);
     userLocation = response.country;
 }, "jsonp");
 
 // Add countries to dropdown 
 
-$.get("http://climatedataapi.worldbank.org/climateweb/rest/v1/country/", function(response) {
+$.get("https://climatedataapi.worldbank.org/climateweb/rest/v1/country/", function(response) {
 	console.log(response);
 })
