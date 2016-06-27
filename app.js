367007fd74a2abdef4979d3570423c003c6b8def
Elizabeth Legris <elizabeth.legris@gmail.com>
2016-06-27 19:37:41 -0400

    change to final project code

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-alert("Hi!");
+var userLocation
+
+// Detect user location
+
+$.get("http://ipinfo.io", function(response) {
+    console.log(response.country);
+    userLocation = response.country;
+}, "jsonp");
+
+// Add countries to dropdown 
+
+$.get("http://climatedataapi.worldbank.org/climateweb/rest/v1/country/", function(response) {
+	console.log(response);
+})
