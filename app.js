a598aaf7a531f8daa17ab57128332e5b8236e4c4
Elizabeth Legris <elizabeth.legris@gmail.com>
2016-06-27 19:58:08 -0400

    trying jsonp wrapper

--- app.js
+++ app.js
@@ -2,13 +2,22 @@ var userLocation
 
 // Detect user location
 
-$.get("https://ipinfo.io", function(response) {
-    console.log(response.country);
-    userLocation = response.country;
-}, "jsonp");
+// $.get("https://ipinfo.io", function(response) {
+//     console.log(response.country);
+//     userLocation = response.country;
+// }, "jsonp");
 
 // Add countries to dropdown 
 
-$.get("https://climatedataapi.worldbank.org/climateweb/rest/v1/country/", function(response) {
-	console.log(response);
-})
+// $.get("https://climatedataapi.worldbank.org/climateweb/rest/v1/country/", function(response) {
+// 	console.log(response);
+// })
+
+function jsonCallback(json){
+  console.log(json);
+}
+
+$.ajax({
+  url: "http://climatedataapi.worldbank.org/climateweb/rest/v1/country/",
+  dataType: "jsonp"
+});
