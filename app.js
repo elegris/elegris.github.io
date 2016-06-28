43efcb1669d3794d9e081c11e02a5ebdcabecf1f
Elizabeth Legris <elizabeth.legris@gmail.com>
2016-06-27 20:36:32 -0400

    try this

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var userLocation
+
 
 // Detect user location
 
@@ -13,11 +13,30 @@ var userLocation
 // 	console.log(response);
 // })
 
-function jsonCallback(json){
-  console.log(json);
-}
+// function jsonCallback(json){
+//   console.log(json);
+// }
 
-$.ajax({
-  url: "https://climatedataapi.worldbank.org/climateweb/rest/v1/country/annualavg/bccr_bcm2_0/b1/tas/2080/2099/fra.json?callback=handler",
-  dataType: "jsonp"
-});
+// $.ajax({
+//   url: "https://climatedataapi.worldbank.org/climateweb/rest/v1/country/annualavg/bccr_bcm2_0/b1/tas/2080/2099/fra.json?callback=handler",
+//   dataType: "jsonp"
+// });
+
+var userLocation
+
+// Detect user location
+
+$.get("http://ipinfo.io", function(response) {
+    console.log(response.country);
+    userLocation = response.country;
+}, "jsonp");
+
+// Add countries to dropdown 
+
+// $.get("http://climatedataapi.worldbank.org/climateweb/rest/v1/country/", function(response) {
+// 	console.log(response);
+// })
+
+$.getJSON('http://cors.io/?u=http://climatedataapi.worldbank.org/climateweb/rest/v1/country/', function(response) {
+	console.log(response);
+})
